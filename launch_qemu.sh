77c70afbc26bb83069ab4891bb4c42a357b6f24b
Alessandro Mandrile <mandrile@feedbackitalia.it>
2024-01-28 11:46:45 +0100

    segfault on read, can register miscdevice but have to allocate groups

--- launch_qemu.sh
+++ launch_qemu.sh
@@ -4,6 +4,6 @@
         -dtb linux-5.15.6/build/arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
         -initrd initramfs_busybox/initramfs.gz \
         -fsdev local,path=pilote_i2c,security_model=mapped,id=mnt \
-        -device virtio-9p-device,fsdev=mnt,mount_tag=mnt
+        -device virtio-9p-device,fsdev=mnt,mount_tag=mnt -s
 
 #mount -t 9p -o trans=virtio mnt /mnt -oversion=9p2000.L,msize=10240
--- pilote_i2c/adxl345.c
+++ pilote_i2c/adxl345.c
@@ -1,19 +1,45 @@
 #include <linux/i2c.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/miscdevice.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/sysfs.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_DEVICES 128
 typedef enum {
     DEVID = 0x00,
     BW_RATE = 0x2C,
     POWER_CTL = 0x2D,
     INT_ENABLE = 0x2E,
     DATA_FORMAT = 0x31,
+    DATAX0 = 0x32,
     FIFO_CTL = 0x38,
 } adxl345_register_t;
 
+struct adxl345_device {
+    int id;
+    int signature;
+    struct miscdevice miscdev;
+};
+
+static bool used_ids[MAX_DEVICES] = {false};
+static unsigned int first_id_available = 0;
+
+static struct attribute* adxl345_attrs [] = {
+    NULL,
+};
+ATTRIBUTE_GROUPS(adxl345);
+ssize_t adxl345_read(struct file *file, char __user *buf, size_t count, loff_t *f_pos);
+
+static struct file_operations adxl345_fileopts = {
+    .read = adxl345_read
+};
 int read_register(struct i2c_client *client, adxl345_register_t address, unsigned char *data);
 int write_register(struct i2c_client *client, adxl345_register_t address, unsigned char *data);
+
 
 int read_register(struct i2c_client *client, adxl345_register_t address, unsigned char *data) {
     int transfered;
@@ -43,8 +69,7 @@ int write_register(struct i2c_client *client, adxl345_register_t address, unsign
     return transfered;
 }
 
-static int adxl345_probe(struct i2c_client *client,
-                         const struct i2c_device_id *id) {
+static int setup_adxl345(struct i2c_client *client) {
     int transfered;
     unsigned char buf;
     printk("adxl345_probe function called\n");
@@ -120,10 +145,52 @@ static int adxl345_probe(struct i2c_client *client,
     pr_info("Set up of the device completed\n");
     return 0;
 }
+static int get_new_id(void);
+static void free_id(int);
+
+static int
+adxl345_probe(struct i2c_client *client,
+              const struct i2c_device_id *id) {
+    int adxl345_id;
+    struct adxl345_device *adxl345_device_;
+    setup_adxl345(client);
+
+    adxl345_device_ = NULL;
+    adxl345_device_ = kmalloc(sizeof(struct adxl345_device), GFP_KERNEL);
+    if (adxl345_device_ == NULL) {
+        return -3;
+    }
+    adxl345_id = get_new_id();
+    if(adxl345_id < 0){
+        return -4;
+    }
+
+    adxl345_device_->id = adxl345_id;
+    adxl345_device_->signature = 
+    adxl345_device_->miscdev.minor = MISC_DYNAMIC_MINOR;
+    adxl345_device_->miscdev.fops = &adxl345_fileopts;
+    adxl345_device_->miscdev.this_device = NULL;
+    adxl345_device_->miscdev.groups = adxl345_groups;
+    adxl345_device_->miscdev.nodename = NULL;
+    
+    //kasprintf(GFP_KERNEL, adxl345_device_->miscdev.name,"adxl345-%d",adxl345_id);
+    adxl345_device_->miscdev.name = kasprintf(GFP_KERNEL,"adxl345-%d",adxl345_id);
+    pr_info("%s\n",adxl345_device_->miscdev.name);
+
+    adxl345_device_->miscdev.parent = &(client->dev);
+    if(adxl345_device_->miscdev.parent == NULL){
+        pr_warn("error\n");
+    }
+    misc_register(&(adxl345_device_->miscdev));
+
+    i2c_set_clientdata(client,(void*)adxl345_device_);
+    return 0;
+}
 static int adxl345_remove(struct i2c_client *client) {
     // Setting Standby mode
     unsigned char buf;
     int transfered;
+    struct adxl345_device* adxl345_device_ ;
 
     transfered = read_register(client, POWER_CTL, &buf);
     if (transfered < 0) {
@@ -138,11 +205,55 @@ static int adxl345_remove(struct i2c_client *client) {
         pr_warn("Error writing adxl345\n");
         return -7;
     }
-
+    
     pr_info("adxl345_remove: device in standby\n");
+    adxl345_device_ = (struct adxl345_device*) i2c_get_clientdata(client);
+    free_id(adxl345_device_->id);
+    misc_deregister(&(adxl345_device_->miscdev));
+    
+    kfree(adxl345_device_->miscdev.name);
+    kfree(adxl345_device_);
     return 0;
 }
 
+ssize_t adxl345_read(struct file *file, char __user *buf, size_t count, loff_t *f_pos){
+    struct miscdevice* miscdev; 
+    struct i2c_client* client;
+    struct adxl345_device* adxl345_device_; 
+    pr_info("Read called!\n");
+    miscdev = (struct miscdevice*)file->private_data;
+    adxl345_device_ = container_of(miscdev,struct adxl345_device,miscdev); 
+    pr_info("adxl345_device_->signature:%d",)
+    char buffer[256];
+    strncpy(buffer,miscdev->name,256);
+    pr_info("miscdevice name: %s\n",buffer);
+    client = container_of(miscdev->parent,struct i2c_client,dev); 
+    read_register(client,DATAX0, buf);
+    return 1;
+    
+}
+
+int get_new_id(void) {
+    // TODO: This function will have to be made atomic later
+    int returned_id;
+    if (first_id_available >= MAX_DEVICES){
+        returned_id = -1;
+    }
+    else{
+        returned_id = first_id_available;
+        used_ids[first_id_available] = true;
+    }
+    
+    while (used_ids[first_id_available] == true || first_id_available >= MAX_DEVICES)
+        first_id_available++;
+    return returned_id;
+}
+static void free_id(int id){
+	// TODO: This function will have to be made atomic later
+    used_ids[id]= false;
+    if(id < first_id_available)
+        first_id_available = id;    
+}
 /* The following list allows the association between a device and its driver
 driver in the case of a static initialization without using
 device tree.
@@ -181,6 +292,7 @@ static struct i2c_driver adxl345_driver = {
     .probe = adxl345_probe,
     .remove = adxl345_remove,
 };
+
 module_i2c_driver(adxl345_driver);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("adxl345 driver");
